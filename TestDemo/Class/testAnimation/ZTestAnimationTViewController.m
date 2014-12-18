f2a241a39fffb8f7ce341fa7789b7359bd67b3af
gypsii001 <gypsii001@perrys-iMac.local>
2014-12-18 14:51:37 +0800

    animation test

--- TestDemo/Class/testAnimation/ZTestAnimationTViewController.m
+++ TestDemo/Class/testAnimation/ZTestAnimationTViewController.m
@@ -34,6 +34,54 @@
     imageView.layer.mask = shapeLayer;
     [self.view addSubview:imageView];
     
+    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(200, 400, 100, 100)];
+    imageView1.image = [UIImage imageNamed:@"test1.png"];
+    
+//    UIBezierPath *path1 = [UIBezierPath bezierPathWithArcCenter:CGPointMake(128/2.f, 128/2.f)
+//                                                        radius:128/2.f startAngle:0 endAngle:M_PI*2.f clockwise:YES];
+    
+    
+    //create path
+    UIBezierPath *path1 = [[UIBezierPath alloc] init];
+    
+//    [path1 moveToPoint:CGPointMake(175, 100)];
+//    [path1 addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
+//    [path1 moveToPoint:CGPointMake(150, 125)];
+//    [path1 addLineToPoint:CGPointMake(150, 175)];
+//    [path1 addLineToPoint:CGPointMake(125, 225)];
+//    [path1 moveToPoint:CGPointMake(150, 175)];
+//    [path1 addLineToPoint:CGPointMake(175, 225)];
+//    [path1 moveToPoint:CGPointMake(100, 150)];
+//    [path1 addLineToPoint:CGPointMake(200, 150)];
+    
+    
+    [path1 moveToPoint:CGPointMake(200, 400)];
+//    [path1 addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
+    [path1 moveToPoint:CGPointMake(300, 400)];
+    [path1 addLineToPoint:CGPointMake(300, 200)];
+//    [path1 addLineToPoint:CGPointMake(125, 225)];
+//    [path1 moveToPoint:CGPointMake(150, 175)];
+//    [path1 addLineToPoint:CGPointMake(175, 225)];
+//    [path1 moveToPoint:CGPointMake(100, 150)];
+//    [path1 addLineToPoint:CGPointMake(200, 150)];
+    
+    //create shape layer
+    CAShapeLayer *shapeLayer1 = [CAShapeLayer layer];
+//    shapeLayer1.strokeColor = [UIColor redColor].CGColor;
+//    shapeLayer1.fillColor = [UIColor clearColor].CGColor;
+//    shapeLayer1.lineWidth = 5;
+//    shapeLayer1.lineJoin = kCALineJoinRound;
+//    shapeLayer1.lineCap = kCALineCapRound;
+    shapeLayer1.path = path1.CGPath;
+    
+    //add it to our view
+//    [self.containerView.layer addSublayer:shapeLayer];
+    
+//    CAShapeLayer *shapeLayer1 = [[CAShapeLayer alloc] init];
+//    shapeLayer1.path = path1.CGPath;
+    imageView1.layer.mask = shapeLayer1;
+    [self.view addSubview:imageView1];
+    
     
     
     UIActivityIndicatorView *rightActivity = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(100, 200, 30.0, 30.0)];
