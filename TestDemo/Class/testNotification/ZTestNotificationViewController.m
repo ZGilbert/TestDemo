032cda310e3f987a516e1c55182c5821ff16fcf3
gypsii001 <gypsii001@admindeiMac.local>
2015-04-02 14:28:35 +0800

    testNotif crash

--- TestDemo/Class/testNotification/ZTestNotificationViewController.m
+++ TestDemo/Class/testNotification/ZTestNotificationViewController.m
@@ -10,6 +10,77 @@
 
 #define TEST_NOTIFICATION @"testNotification"
 
+@interface ZPoster : NSObject
+
+@end
+
+@implementation ZPoster
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [self performSelectorInBackground:@selector(postNotification) withObject:nil];
+    }
+    
+    return self;
+}
+
+- (void)postNotification
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:TEST_NOTIFICATION object:nil];
+}
+
+@end
+
+#pragma mark - Observer
+
+@interface Observer : NSObject
+{
+    ZPoster  *_poster;
+}
+
+@property (nonatomic, assign) NSInteger i;
+
+@end
+
+@implementation Observer
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        _poster = [[ZPoster alloc] init];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleNotification:) name:TEST_NOTIFICATION object:nil];
+    }
+    
+    return self;
+}
+
+- (void)handleNotification:(NSNotification *)notification
+{
+    NSLog(@"handle notification begin");
+    sleep(1);
+    NSLog(@"handle notification end");
+    
+    self.i = 10;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    NSLog(@"Observer dealloc");
+}
+
+@end
+
+#pragma mark - viewcontroller
 @interface ZTestNotificationViewController () <NSMachPortDelegate>
 
 @end
@@ -17,6 +88,7 @@
 @implementation ZTestNotificationViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
@@ -40,7 +112,7 @@
     
     
     // part II ====================== start ====================== //
-    NSLog(@"current thread = %@", [NSThread currentThread]);
+    /*NSLog(@"current thread = %@", [NSThread currentThread]);
     
     // 初始化
     self.notifications = [[NSMutableArray alloc] init];
@@ -59,8 +131,13 @@
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [[NSNotificationCenter defaultCenter] postNotificationName:TEST_NOTIFICATION object:nil userInfo:nil];
-    });
+    });*/
     // part II ====================== end ====================== //
+    
+    
+    // part III ====================== start ====================== //
+     __autoreleasing Observer *observer = [[Observer alloc] init];
+    // part III ===================== end ====================== //
     
 }
 
