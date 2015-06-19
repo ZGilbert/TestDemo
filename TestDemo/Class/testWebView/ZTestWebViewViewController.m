5f5bfdabd05a18edb7836adf85912b1ff03e24d5
gilbert <haiob88@163.com>
2015-06-19 15:02:37 +0800

    t

--- TestDemo/Class/testWebView/ZTestWebViewViewController.m
+++ TestDemo/Class/testWebView/ZTestWebViewViewController.m
@@ -21,17 +21,41 @@
     self.navigationItem.title = @"积分使用规则";
     
     self.view.backgroundColor = [UIColor clearColor];
-    
+
     self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, APP_SCREEN_WIDTH, APP_SCREEN_HEIGHT)];
-//    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://openapi.idc.nonobank.com/nono-web/agreement/getCreditServiceAgreement?request={\"openId\":\"168766\",\"merchant\":\"10000\",\"msgKey\":\"A2dKIgmd75gfRo3dLwuQ2w==\"}", nil]];
-//    [self.webView loadRequest:request];
+    
+//    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://openapi.idc.nonobank.com/nono-web/agreement/getCreditServiceAgreement?request={'openId':'168766','merchant':'10000','msgKey':'A2dKIgmd75gfRo3dLwuQ2w=='}"]];
+    NSDictionary *dict1;
+    
+//    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://openapi.idc.nonobank.com/nono-web/agreement/getCreditServiceAgreement?request=%@", dict1]];
+    
+    NSString *urlString = [[dict1 objectForKey:@"map"] objectForKey:@"agreementUrl"];
+    //不转义
+    NSString *encodedString = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes
+                                                            (kCFAllocatorDefault,
+                                                             (CFStringRef)urlString,
+                                                             (CFStringRef)@"!$&'()*+,-./:;=?@_~%#[]", NULL,
+                                                              kCFStringEncodingUTF8));
+    
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:encodedString]];
+    
+    [self.webView loadRequest:request];
     
 //    if (self.tabBarController == nil){
 //        self.webViewContraint.constant = 0;
 //    }
     
     [self.view addSubview:self.webView];
+
+    NSDictionary *dict = @{@"aaa":@"bbb", @"ccc":@"ddd"};
+    NSString *str = dict[@"aaa"];
+    ZLOG(@"str ==> %@", str);
+
+
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
